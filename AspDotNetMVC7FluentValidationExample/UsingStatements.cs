55adc391de407526f9ab3f83669030c0ad664d62
Simon Gilbert <sah.gilbert@gmail.com>
2022-11-12 17:38:04 +0000

    Update UsingStatements.cs

--- AspDotNetMVC7FluentValidationExample/UsingStatements.cs
+++ AspDotNetMVC7FluentValidationExample/UsingStatements.cs
@@ -1,3 +1,5 @@
-﻿global using Microsoft.AspNetCore.Mvc;
+﻿global using FluentValidation;
 global using FluentValidation.AspNetCore;
+global using Microsoft.AspNetCore.Mvc;
 global using AspDotNetMVC7FluentValidationExample.ViewModels;
+global using AspDotNetMVC7FluentValidationExample.Validation;
