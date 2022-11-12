a69c4dfed18dd80e63399beb35ceedb2c581eae7
Simon Gilbert <sah.gilbert@gmail.com>
2022-11-12 16:56:09 +0000

    Create ExampleCreateViewModel.cs

---
+++ AspDotNetMVC7FluentValidationExample.ViewModels/ExampleCreateViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace AspDotNetMVC7FluentValidationExample.ViewModels;
+
+public sealed class ExampleCreateViewModel
+{
+    [Display(Name = "First Line of Address")]
+    public string FirstLineOfAddress { get; set; }
+
+    public string PropertyPrice { get; set; }
+
+    [Display(Name = "Currency")]
+    public string Currency { get; set; }
+
+    public IEnumerable<SelectListItem> Currencies
+    {
+        get
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem { Text = "GBP", Value = "GBP" },
+                new SelectListItem { Text = "EUR", Value = "EUR" },
+                new SelectListItem { Text = "USD", Value = "USD" },
+            };
+        }
+    }
+
+    [Display(Name = "Description")]
+    public string Description { get; set; }
+}
