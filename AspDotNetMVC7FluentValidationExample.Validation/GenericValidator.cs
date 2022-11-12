4f1aee2c7371676fe24195be44ca0e93827faaf8
Simon Gilbert <sah.gilbert@gmail.com>
2022-11-12 17:02:52 +0000

    Create GenericValidator.cs

---
+++ AspDotNetMVC7FluentValidationExample.Validation/GenericValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace AspDotNetMVC7FluentValidationExample.Validation;
+
+public abstract class GenericValidator<T> : AbstractValidator<T>
+{
+    protected override bool PreValidate(ValidationContext<T> validationContext, ValidationResult validationResult)
+    {
+        if (validationContext.InstanceToValidate == null)
+        {
+            validationResult.Errors.Add(new ValidationFailure("", "Please submit a non-null model"));
+
+            return false;
+        }
+
+        return true;
+    }
+
+    protected bool IsValidSelectedValue(string selectedValue, IEnumerable<SelectListItem> selectList)
+    {
+        var selectListItem = selectList
+            .Where(x => x.Value == selectedValue)
+            .FirstOrDefault();
+
+        if (selectListItem != null && selectListItem.Value == selectedValue)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
