55b537ff8703b52188113170f5a54b886c939b0e
Simon Gilbert <sah.gilbert@gmail.com>
2022-11-12 17:06:35 +0000

    Update ExampleController.cs

--- AspDotNetMVC7FluentValidationExample/Controllers/ExampleController.cs
+++ AspDotNetMVC7FluentValidationExample/Controllers/ExampleController.cs
@@ -1,11 +1,40 @@
-﻿using System;
-namespace AspDotNetMVC7FluentValidationExample.Controllers
+﻿namespace AspDotNetMVC7FluentValidationExample.Controllers;
+
+public class ExampleController : Controller
 {
-	public class ExampleController
-	{
-		public ExampleController()
-		{
-		}
-	}
+    [HttpGet]
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public ActionResult Create()
+    {
+        return View(new ExampleCreateViewModel());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(ExampleCreateViewModel exampleCreateViewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            bool successfullyStoredInDatabase = true;
+
+            if (successfullyStoredInDatabase)
+            {
+                return RedirectToAction("Success", "Example");
+            }
+            else
+            {
+                return View("Create", exampleCreateViewModel);
+            }
+        }
+
+        return View("Create", exampleCreateViewModel);
+    }
 }
+
+
 
