4f7c979ab4cac080daa77be646b234a02a856e26
Simon Gilbert <sah.gilbert@gmail.com>
2022-11-12 17:49:51 +0000

    Create ValidationExtensions.cs

---
+++ AspDotNetMVC7FluentValidationExample.Validation.Extensions/ValidationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public static class ValidationExtensions
+{
+    public static void AddToModelState(this ValidationResult result, ModelStateDictionary modelState)
+    {
+        foreach (var error in result.Errors)
+        {
+            modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+        }
+    }
+}
