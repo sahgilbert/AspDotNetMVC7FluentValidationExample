d6f8cdea585f64d492ebdaccb570c2c8962de78b
Simon Gilbert <sah.gilbert@gmail.com>
2022-11-12 17:52:55 +0000

    Update UsingStatements.cs

--- AspDotNetMVC7FluentValidationExample.Validation.Extensions/UsingStatements.cs
+++ AspDotNetMVC7FluentValidationExample.Validation.Extensions/UsingStatements.cs
@@ -1,2 +1,3 @@
-﻿global using System.ComponentModel.DataAnnotations;
+﻿global using FluentValidation.Results;
+global using System.ComponentModel.DataAnnotations;
 global using Microsoft.AspNetCore.Mvc.ModelBinding;
